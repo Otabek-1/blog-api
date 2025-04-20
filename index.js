2bc3ec89ffb141e10d07839da159c4f5b74c8042
Otabek <burhonovotoabek5@gmail.com>
2025-04-20 20:00:08 +0500

    created HTTP endpoints

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("Welcome to blog API!");
+// GET
+app.get("/hello", (req,res)=>{
+    res.status(200).send("Hello, world!");
+})
+
+//POST
+app.post("/user",(req,res)=>{
+    const {name} = req.body;
+    res.status(201).json({message:`user ${name} created!`});
+})
+
+//404
+app.use((req,res)=>{
+    res.status(404).json({error:"Route not found!"})
 })
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, ()=>console.log(`Server is running on port: ${PORT}`));
+app.listen(PORT, ()=>console.log(`Server listening on http://localhost:${PORT}`));
